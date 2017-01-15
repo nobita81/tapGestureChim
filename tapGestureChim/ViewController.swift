35cee8f1e2c1803ad0792df337dddb9a6bf92223
nobita81 <Dr@Drs-iMac.local>
2017-01-15 20:41:57 +0700

    Repair Fix Label Update
    Image Chim Reset Location

--- tapGestureChim/ViewController.swift
+++ tapGestureChim/ViewController.swift
@@ -17,18 +17,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var cuc1: UIView!
     @IBOutlet weak var cuc2: UIView!
     
+    @IBOutlet weak var lblTen: UILabel!
+    @IBOutlet weak var lblDiem: UILabel!
     
-    
+    var diem:Int = 0
     
     var timer:Timer!
     var timerChim:Timer!
     let thoigian:Double = 10
-    
+    var dequy:Bool = true
     
     
     func tapChim(_ sender: Any) {
         if imgChim.frame.origin.y > imgChim.frame.size.height {
-            imgChim.frame.origin.y -= 20
+            imgChim.frame.origin.y -= 30
+            
         }
     }
     
@@ -40,7 +43,9 @@ class ViewController: UIViewController {
             self.imgView.layer.transform = tranfomr
         }, completion: {(true) in
             self.imgView.layer.transform = CATransform3DIdentity
-            self.manhinhdichuyen()
+            if self.dequy == true {
+                self.manhinhdichuyen()
+            }
         })
     }
     
@@ -49,9 +54,13 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
             if self.imgChim.frame.origin.y <= self.view.frame.size.height - self.imgChim.frame.size.height{
                 self.imgChim.frame.origin.y += 50
+                
             }
-        }, completion: { (false) in
-            self.roiChim()
+        }, completion: { (true) in
+            if self.dequy == true {
+                
+                self.roiChim()
+            }
         })
         
     }
@@ -62,37 +71,52 @@ class ViewController: UIViewController {
         /// **Tạo Biến** Gom Code Dễ Sửa
         let khoangcach2cuc:UInt32 = 150
         
+        /// Random Heigh Cuc1
+        let rd1 = arc4random_uniform(400) + 100
+        /// Heigh Cuc2 = Heigh View - (Heigh Cuc1(rd1) + khoảng cách 2 Cục)
+        let rd2 = UInt32(view.frame.size.height) - (rd1 + khoangcach2cuc)
+        
         if self.cuc1.frame.origin.x < self.view.frame.size.width - self.cuc1.frame.size.width {
+            
             
             UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
                 self.cuc1.frame.origin.x += 50
                 self.cuc2.frame.origin.x += 50
+                
             }, completion: { (true) in
-                self.randomOng()
+                if self.dequy == true {
+                    self.randomOng()
+                }
             })
             
+            
             if cuc2.frame.origin.x + imgChim.frame.size.width >= imgChim.frame.origin.x && imgChim.frame.origin.x >= cuc2.frame.origin.x && (imgChim.frame.origin.y + imgChim.frame.size.height <= cuc1.frame.size.height || imgChim.frame.origin.y + imgChim.frame.size.height >= cuc1.frame.size.height + CGFloat(khoangcach2cuc))
-                
             {
                 imgChim.frame.origin.x = (cuc2.frame.origin.x + imgChim.frame.size.width)
-
+                
+                if imgChim.frame.origin.x >= view.frame.size.width {
+                    lblDiem.text = "Bạn Đã Thua!! Điểm bạn là \(diem)"
+                    dequy = false
+                }
             }
+            
+            
         } else {
             
             /// Vị trí Cuc1,Cuc2 Xuất Hiện
             self.cuc1.frame.origin.x = (-(self.cuc1.frame.size.width))
             self.cuc2.frame.origin.x = (-(self.cuc2.frame.size.width))
             
-            /// Random Heigh Cuc1
-            let rd1 = arc4random_uniform(400)
-            /// Heigh Cuc2 = Heigh View - (Heigh Cuc1(rd1) + khoảng cách 2 Cục)
-            let rd2 = UInt32(view.frame.size.height) - (rd1 + khoangcach2cuc)
-            
+           
             
             cuc1.frame.size.height = CGFloat(rd1)
             
             cuc2.frame.origin.y = cuc1.frame.size.height + CGFloat(khoangcach2cuc)
             cuc2.frame.size.height = CGFloat(rd2)
+            
+            diem += 1
+            print(diem)
+            //lblDiem.text = String(diem)
             
             /// Gọi Để Func Chạy Lại(Thay Timer)
             self.randomOng()
@@ -104,7 +128,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        lblTen.text = "Hoàng Nguyễn"
+        lblDiem.text = String(diem)
+        
         //timer = Timer.scheduledTimer(timeInterval: thoigian + 0.2, target: self, selector: #selector(ViewController.manhinhdichuyen), userInfo: nil, repeats: true)
+        
         
         manhinhdichuyen()
         roiChim()
